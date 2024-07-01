da9a0ed9767f806a426b720fdb425b13da2e9ef6
Binwen Wu <2020302131043@whu.edu.cn>
2024-07-01 15:29:42 +0800

    modify: default shell

--- config/launch.lua
+++ config/launch.lua
@@ -6,7 +6,7 @@ local options = {
 }
 
 if platform.is_win then
-   options.default_prog = { 'pwsh' }
+   options.default_prog = { 'powershell' }
    options.launch_menu = {
       { label = 'PowerShell Core', args = { 'pwsh' } },
       { label = 'PowerShell Desktop', args = { 'powershell' } },
@@ -26,7 +26,7 @@ elseif platform.is_mac then
       { label = 'Zsh', args = { 'zsh', '-l' } },
    }
 elseif platform.is_linux then
-   options.default_prog = { 'fish', '-l' }
+   options.default_prog = { 'zsh', '-l' }
    options.launch_menu = {
       { label = 'Bash', args = { 'bash', '-l' } },
       { label = 'Fish', args = { 'fish', '-l' } },

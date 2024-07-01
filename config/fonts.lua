28ceb4ffe1f9bb5c91ab14e9a83578d7b46f7eb9
Binwen Wu <2020302131043@whu.edu.cn>
2024-07-01 15:47:40 +0800

    modify: line height

--- config/fonts.lua
+++ config/fonts.lua
@@ -1,15 +1,19 @@
 local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
+-- font style
 local font = 'CaskaydiaMono Nerd Font' 
--- 默认：local font = 'JetBrainsMono Nerd Font'
 
+-- font size
 local font_size = platform().is_mac and 14 or 11
+
+-- line height
+local line_height = platform().is_mac and 1.15 or 1.15
 
 return {
    font = wezterm.font(font),
    font_size = font_size,
-
+   line_height = line_height,
    --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
    freetype_load_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
    freetype_render_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
